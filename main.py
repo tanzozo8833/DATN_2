9b5009e817ef923a87bdbf66a68f27bc633a80cc
tanzozo8833 <hoangductan733@gmail.com>
2026-05-03 19:39:41 +0700

    ok version 3.1

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ def main():
         'device': 'cuda' if torch.cuda.is_available() else 'cpu',
         'lr': 1e-4,
         'epochs': 100,
-        'batch_size': 8,
+        'batch_size': 32,
         'project_name': 'Light-MSKA-SLR'
     }
 
